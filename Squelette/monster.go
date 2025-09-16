91268a318b20a8e25d3cd1fce701655f0b08968f
Chronos8001 <antoine.rudnianyn@ynov.com>
2025-09-16 10:25:13 +0200

    les stat a skelly

---
+++ Squelette/monster.go
@@ -0,0 +1,26 @@
+package main
+
+type Attack struct {
+	Name      string
+	Damage    int
+	HitChance float64
+}
+
+type Monster struct {
+	Name      string
+	MaxHP     int
+	CurrentHP int
+	Attacks   []Attack
+}
+
+func NewSkelly() Monster {
+	return Monster{
+		Name:      "Skelly",
+		MaxHP:     20,
+		CurrentHP: 20,
+		Attacks: []Attack{
+			{Name: "Épée", Damage: 3, HitChance: 0.90},
+			{Name: "Lancé d'os", Damage: 3, HitChance: 0.75},
+		},
+	}
+}
