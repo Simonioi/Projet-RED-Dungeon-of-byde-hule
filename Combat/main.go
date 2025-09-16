43ba9c9bcb33304f86e1ba824147d99e13601473
Chronos8001 <antoine.rudnianyn@ynov.com>
2025-09-16 15:33:14 +0200

    je suis éclaté, j'en peux plus de ces conneries je vous jure. bisou, Time

--- Combat/main.go
+++ Combat/main.go
@@ -6,17 +6,30 @@ import (
 	"dungeon/mimic"
 )
 
-func Attack(attacker *personnage.Character, defender *mimic.Monster) {
+func Attack(player *personnage.Character, enemy *mimic.Monster) {
     totalDamage := 0
-    for _, attack := range attacker.Attacks {
+    for _, attack := range player.Attacks {
         totalDamage += attack.Damage
     }
     if totalDamage < 0 {
         totalDamage = 0
     }
-    defender.CurrentHP -= totalDamage
-    fmt.Printf("%s attaque %s et inflige %d dégâts !\n", attacker.Name, defender.Name, totalDamage)
+    enemy.CurrentHP -= totalDamage
+    fmt.Printf("%s attaque %s et inflige %d dégâts !\n", player.Name, enemy.Name, totalDamage)
 }
+
+func enAttack(enemy *mimic.Monster, player *personnage.Character) {
+    totalDamage := 0
+    for _, attack := range enemy.Attacks {
+        totalDamage += attack.Damage
+    }
+    if totalDamage < 0 {
+        totalDamage = 0
+    }
+    player.CurrentHP -= totalDamage
+    fmt.Printf("%s attaque %s et inflige %d dégâts !\n", enemy.Name, player.Name, totalDamage)
+}
+
 
 func Battle(player *personnage.Character, enemy *mimic.Monster) {
     for player.CurrentHP > 0 && enemy.CurrentHP > 0 {
@@ -28,7 +41,7 @@ func Battle(player *personnage.Character, enemy *mimic.Monster) {
         }
 
         fmt.Println("\n--- Tour de l'ennemi ---")
-        Attack(player, enemy)
+        enAttack(enemy, player)
         if player.CurrentHP <= 0 {
             fmt.Println(player.Name, "est vaincu !")
             break
